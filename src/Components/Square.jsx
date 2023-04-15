7129385be7410525f88912c8525fd6e86db39ff3
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2023-04-16 00:32:40 +0200

    Lifting state up

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react';
+function Square({ value, onSquareClick }) {
 
-
-function Square() {
-
-    const [value, setValue] = useState(null);
-
-    function handleClick(){
-        setValue('X')
-    }
     return ( 
-        <button className="square" onClick={handleClick}>{value}</button>
+        <button className="square" onClick={onSquareClick}>{value}</button>
     );
 }
 
